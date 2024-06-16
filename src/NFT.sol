990c04b25d9cccf3d6286f97abd26688dfbf5d54
hiromaru05s <maruru836818@gmail.com>
2024-06-16 04:19:29 -0400

    revised some codes in nft.sol

--- src/NFT.sol
+++ src/NFT.sol
@@ -4,15 +4,27 @@ pragma solidity ^0.8.0;
 import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-abstract contract AbstractNFT is ERC721, Ownable {
-    uint256 public nextTokenId;
+contract NFT is ERC721, Ownable {
+    uint256 private _tokenIdCounter;
 
-    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
-        nextTokenId = 0;
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) Ownable() {
+        _tokenIdCounter = 1; // トークンIDの初期値を設定
     }
 
-    function mint(address to) external virtual onlyOwner {
-        _safeMint(to, nextTokenId);
-        nextTokenId++;
+    /**
+     * @dev Public function to mint a new token. Only the owner can call this function.
+     * @param to The address that will receive the minted token.
+     */
+    function mint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+        _safeMint(to, tokenId);
+    }
+
+    /**
+     * @dev Override _baseURI to return the base URI for computing {tokenURI}.
+     */
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "https://api.example.com/metadata/";
     }
 }
